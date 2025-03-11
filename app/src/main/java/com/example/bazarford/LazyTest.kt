29119d94c6aeecd217a28221c4f7df9e02b2ed8a
soumya-99 <soumyadeepmondal1234@gmail.com>
2025-03-11 12:50:39 +0530

    first commit

---
+++ app/src/main/java/com/example/bazarford/LazyTest.kt
@@ -0,0 +1,193 @@
+package com.example.bazarford
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+data class Contact (
+    val name: String,
+    val phoneNumber: String
+)
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LazyTest() {
+
+    val contacts = remember {
+        listOf(
+            Contact(
+                name = "Soumyadeep Mondal",
+                phoneNumber = "8910792003"
+            ),
+            Contact(
+                name = "Hello",
+                phoneNumber = "8910792003"
+            ),
+            Contact(
+                name = "Hiii",
+                phoneNumber = "8910792003"
+            ),
+            Contact(
+                name = "SOme name",
+                phoneNumber = "8910792003"
+            ),
+            Contact(
+                name = "fsd Mondal",
+                phoneNumber = "8910792003"
+            ),
+            Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ), Contact(
+                name = "Avisdhfo",
+                phoneNumber = "8910792003"
+            ),
+        )
+    }
+
+//    LazyColumn(
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//
+//        items(contacts) {
+//            contact ->
+//            Card(
+//                modifier = Modifier.fillMaxWidth().padding(16.dp)
+//            ) {
+//                Column(
+//                    modifier = Modifier.padding(12.dp)
+//                ) {
+//                    Text(text = contact.name)
+//                    Text(text = contact.phoneNumber)
+//                }
+//            }
+//        }
+//
+//    }
+
+
+    LazyRow (
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        items(contacts) {
+                contact ->
+            Card(
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(12.dp)
+                ) {
+                    Text(text = contact.name)
+                    Text(text = contact.phoneNumber)
+                }
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/bazarford/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.bazarford
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bazarford.ui.theme.BazarFordTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BazarFordTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LazyTest()
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Test() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Hello World",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(100.dp).clip(RoundedCornerShape(15.dp))
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.weight(1f),
+                    ) {
+                        Text(text = "Hell")
+                    }
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "Heaven")
+                    }
+                }
+            }
+        }
+    }
+}
